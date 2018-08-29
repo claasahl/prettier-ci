756af749d27bcd697c551873971b2039f4fa4d98
Claas Ahlrichs <claasahl@gmail.com>
2018-08-29 06:37:35 +0200

    initial import of sample test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,35 @@
 // You can import your modules
 // import index from '../src/index'
+import {Application} from "probot";
+import myProbotApp from "../src/index";
+
+import pushEvent from './events/push.json';
+
+describe('My Probot app', () => {
+  let app, github
+
+  beforeEach(() => {
+    app = new Application()
+    // Initialize the app based on the code from index.js
+    app.load(myProbotApp)
+    // This is an easy way to mock out the GitHub API
+    github = {
+      issues: {
+        createComment: jest.fn().mockReturnValue(Promise.resolve({}))
+      }
+    }
+    // Passes the mocked out GitHub API into out app instance
+    app.auth = () => Promise.resolve(github)
+  })
+
+  test('creates a comment when an issue is opened', async () => {
+    // Simulates delivery of an issues.opened webhook
+    await app.receive(pushEvent)
+
+    // This test passes if the code in your index.js file calls `context.github.issues.createComment`
+    expect(github.issues.createComment).toHaveBeenCalled()
+  })
+})
 
 test('that we can run tests', () => {
   // your real tests go here
@@ -8,3 +38,6 @@ test('that we can run tests', () => {
 
 // For more information about using TypeScript in your tests, Jest recommends:
 // https://github.com/kulshekhar/ts-jest
+
+// For more information about testing with probot:
+// https://probot.github.io/docs/testing/
