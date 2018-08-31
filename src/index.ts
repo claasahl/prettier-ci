57e01a08d46bae49bc50c3ab0a839b7068818bc0
Claas Ahlrichs <claasahl@gmail.com>
2018-08-31 06:40:26 +0200

    inspect modified files and format them if necessary

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { Application } from 'probot'
+import { ReposGetContentParams, Response, ReposUpdateFileParams } from '@octokit/rest';
+import * as prettier from "prettier";
+import atob from 'atob';
+import btoa from 'btoa';
 
 // FIXME this just to keep "eslint-plugin-typescript" from complaining about unused references
 Application.toString()
@@ -25,6 +29,36 @@ export = (app: Application) => {
     context.log(JSON.stringify(context, null, 2))
     const files = getFiles(context.payload);
     context.log(files);
+
+    const owner = context.payload.repository.owner.name;
+    const repo = context.payload.repository.name;
+    const ref = context.payload.ref;
+    for(const path of files) {
+      const params: ReposGetContentParams = {ref, path, owner, repo};
+      context.log("Inspecting file", params);
+      const options = {filepath: path};
+
+      const contentResponse: Response<any> = await context.github.repos.getContent(params);
+      if(contentResponse.status === 200 && contentResponse.data.encoding === "base64") {
+        context.log("Retrieved file", params);
+      const content = atob(contentResponse.data.content);
+
+      if(!prettier.check(content, options)) {
+        context.log("Found formatting issues in file", params);
+        
+        const formatted: string = prettier.format(content, options);
+        const message = "a sprinkle eye candy";
+        const sha = contentResponse.data.sha;
+        const bla: ReposUpdateFileParams = {content: btoa(formatted), owner, repo, path, message, branch: ref, sha};
+        const updateResponse = await context.github.repos.updateFile(bla);
+        context.log(updateResponse)
+      } else {
+        context.log("No formatting issues in file", params);
+      }
+    } else {
+      context.log("Failed to retrieve file or encoding is not base64", params, contentResponse);
+    }
+    }
   })
 
   // For more information on building apps:
