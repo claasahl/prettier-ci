cb7d2a8c8bb81bea0d7df32a840e7355a65f9340
Claas Ahlrichs <claasahl@gmail.com>
2018-09-08 15:06:42 +0200

    simplified create/update of gh checks

--- src/actions/checks.ts
+++ src/actions/checks.ts
@@ -1,34 +1,22 @@
-import { ChecksCreateParams, ChecksCreateResult, ChecksUpdateParams, ChecksUpdateResult } from '../types'
+import { ChecksCreateParams, ChecksUpdateParams } from '../types'
 
 const defaultChecksCreateParams = {
   name: 'Prettier',
   status: 'queued' as 'queued'
 }
 
-export async function create (params: ChecksCreateParams): Promise<ChecksCreateResult> {
+export async function create (params: ChecksCreateParams): Promise<void> {
   const mixedParams = { ...defaultChecksCreateParams, ...params }
   const { github, log, ...createParams } = mixedParams
-  try {
-    await github.checks.create(createParams)
-    return {params: mixedParams, created: true}
-  } catch (error) {
-    log('error while creating check_run', createParams, error)
-  }
-  return {params: mixedParams, created: false}
+  await github.checks.create(createParams)
 }
 
 const defaultChecksUpdateParams = {
   name: 'Prettier'
 }
 
-export async function update (params: ChecksUpdateParams): Promise<ChecksUpdateResult> {
+export async function update (params: ChecksUpdateParams): Promise<void> {
   const mixedParams = { ...defaultChecksUpdateParams, ...params }
   const { github, log, ...upatedParams } = mixedParams
-  try {
-    await github.checks.update(upatedParams)
-    return {params: mixedParams, updated: true}
-  } catch (error) {
-    log('error while updating check_run', upatedParams, error)
-  }
-  return {params: mixedParams, updated: false}
+  await github.checks.update(upatedParams)
 }
