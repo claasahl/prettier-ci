fac9b896dd00f0668e3254a7f6f279bbffee5bff
Claas Ahlrichs <claasahl@gmail.com>
2018-09-09 12:36:46 +0200

    removed more rxjs-based implementations

--- src/reactivex/rxPrettier.ts
+++
@@ -1,8 +0,0 @@
-import * as rx from "rxjs"
-import * as prettier from "prettier"
-
-export namespace rxPrettier {
-    export function check(source: string, options?: prettier.Options): rx.Observable<boolean> {
-        return rx.of(prettier.check(source, options))
-    }
-}
