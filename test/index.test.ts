dbed80b1318579d5a6746eda76875dd2ff24021d
Claas Ahlrichs <claasahl@gmail.com>
2018-09-09 19:01:13 +0200

    added test cases for file-analysis

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,6 +7,11 @@ import * as gh from "@octokit/rest";
 import checkSuiteRequestedEvent from "./events/check_suite/requested.json"
 import checkSuiteRerequestedEvent from "./events/check_suite/rerequested.json"
 import checkRunRerequestedEvent from "./events/check_run/rerequested.json"
+import checkRunCreatedEvent from "./events/check_run/created.json"
+
+import reposCompareCommits_200 from "./responses/repos/compareCommits__200.json"
+import reposgetContent_200 from "./responses/repos/getContent__200.json"
+
 import { CHECKS_NAME } from '../src/utils';
 
 describe('tests conditions for triggering a file-analysis', () => {
@@ -46,6 +51,50 @@ describe('tests conditions for triggering a file-analysis', () => {
 
     const params: gh.ChecksCreateParams = { owner: "claasahl", repo: "prettiest-bot", name: CHECKS_NAME, head_sha: "fad5b3a8a9602fd31279d8c707e5a0de3c4cd640" };
     expect(github.checks.create).toHaveBeenCalledWith(params)
+  })
+})
+
+describe('tests for file-analysis', () => {
+  let app, github
+
+  beforeEach(() => {
+    app = new Application()
+    // Initialize the app based on the code from index.js
+    app.load(myProbotApp)
+    // This is an easy way to mock out the GitHub API
+    github = {
+      checks: {
+        create: jest.fn().mockReturnValue(Promise.resolve({})),
+        update: jest.fn().mockReturnValue(Promise.resolve({}))
+      },
+      repos: {
+        compareCommits: jest.fn().mockReturnValue(Promise.resolve(reposCompareCommits_200)),
+        getContent: jest.fn().mockReturnValue(Promise.resolve(reposgetContent_200))
+      }
+    }
+    // Passes the mocked out GitHub API into out app instance
+    app.auth = () => Promise.resolve(github)
+  })
+
+  test('mark check as "in_progress" when check_run is created', async () => {
+    await app.receive(checkRunCreatedEvent)
+
+    const params: gh.ChecksUpdateParams = { owner: "claasahl", repo: "prettiest-bot", check_run_id: "15222485", status: "in_progress"};
+    expect(github.checks.update).toHaveBeenCalledWith(params)
+  })
+
+  test('compares before/after commits when check_run is created', async () => {
+    await app.receive(checkRunCreatedEvent)
+
+    const params: gh.ReposCompareCommitsParams = { owner: "claasahl", repo: "prettiest-bot", base: "923547128e3e9fe26c4192bb45fcf125dae92c4b", head: "52c67357a6b1858226c131d13d7d44e8303fd426"};
+    expect(github.repos.compareCommits).toHaveBeenCalledWith(params)
+  })
+
+  test('retrieves contents for version that was present when check_run is created', async () => {
+    await app.receive(checkRunCreatedEvent)
+
+    const params: gh.ReposGetContentParams = { owner: "claasahl", repo: "prettiest-bot", path: "test/utils.test.ts", ref: "7d6e8ee3e4c26b6d5d305fa3fe985ddf7c0f87ea"};
+    expect(github.repos.getContent).toHaveBeenCalledWith(params)
   })
 })
 
