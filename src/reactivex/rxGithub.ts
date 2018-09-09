7e33ec168fb5e3f12ead1f62772c9ca4de6a4372
Claas Ahlrichs <claasahl@gmail.com>
2018-09-09 12:06:03 +0200

    re-implemented promise-based workflow

--- src/reactivex/rxGithub.ts
+++ src/reactivex/rxGithub.ts
@@ -4,6 +4,19 @@ import { Context } from "probot";
 import { Projection } from "../types";
 
 
+export namespace promiseChecks {
+    export async function create(context: Context, project: Projection<gh.ChecksCreateParams>) {
+        const params = project(context)
+        const response = await context.github.checks.create(params)
+        return {context, response}
+    }
+    export async function update(context: Context, project: Projection<gh.ChecksUpdateParams>) {
+        const params = project(context)
+        const response = await context.github.checks.update(params)
+        return {context, response}
+    }
+}
+
 export namespace rxChecks {
     export function create(context: Context, project: Projection<gh.ChecksCreateParams>) {
         const params = project(context)
@@ -16,6 +29,19 @@ export namespace rxChecks {
         const ctx = Rx.of(context)
         const response = Rx.from(context.github.checks.update(params))
         return Rx.combineLatest(ctx, response, (context, response) => ({context, response}))
+    }
+}
+
+export namespace promiseRepos {
+    export async function compareCommits(context: Context, project: Projection<gh.ReposCompareCommitsParams>) {
+        const params = project(context)
+        const response = await context.github.repos.compareCommits(params)
+        return {context, response}
+    }
+    export async function getContent(context: Context, project: Projection<gh.ReposGetContentParams>) {
+        const params = project(context)
+        const response = await context.github.repos.getContent(params)
+        return {context, response}
     }
 }
 
