56dc6ab4b86eed5d4baed220dfc3c90eaa97dfa9
Claas Ahlrichs <claasahl@gmail.com>
2018-09-09 12:12:56 +0200

    largely removed rxjs-based implementation

--- src/events/check_run.ts
+++ src/events/check_run.ts
@@ -1,39 +1,24 @@
-import * as rx from "rxjs";
 import * as gh from "@octokit/rest";
-import { rxRepos, rxChecks, promiseChecks, promiseRepos } from "../reactivex/rxGithub"
-import { rxPrettier }  from "../reactivex/rxPrettier"
+import { promiseChecks, promiseRepos } from "../reactivex/rxGithub"
 import { Context } from 'probot'
 import * as prettier from "prettier"
 
-import { map, flatMap, reduce, tap } from 'rxjs/operators';
 import { CHECKS_NAME } from '../utils';
 import { Projection, Overwrite } from '../types';
 import btoa from "btoa";
 
-export async function createdd(context: Context): Promise<Context> {
-  await _markCheckAsInProgress(context);
-  const {files} = await _fetchModifiedFiles(context);
+export async function created(context: Context): Promise<Context> {
+  await markCheckAsInProgress(context);
+  const {files} = await fetchModifiedFiles(context);
   const results: {file: string, passed: boolean}[] = [];
   for(const file of files) {
-    const {content} = await _fetchContent({context, file})
-    const {passed} = await _checkContent({context, file, content})
+    const {content} = await fetchContent({context, file})
+    const {passed} = await checkContent({context, file, content})
     results.push({file, passed})
   }
   const report = asReport(results)
-  await _markCheckAsCompleted({context, report})
+  await markCheckAsCompleted({context, report})
   return context
-}
-
-export function created(context: Context): rx.Observable<Context> {
-  return rx.of(context).pipe(
-    markCheckAsInProgress,
-    fetchModifiedFiles,
-    // skipBinaryFiles
-    fetchContent,
-    checkContent,
-    compileReport,
-    markCheckAsCompleted
-  );
 }
 
 function created2ChecksUpdateParams(params: Partial<gh.ChecksUpdateParams>): Projection<gh.ChecksUpdateParams> {
@@ -61,33 +46,18 @@ function created2ReposGetContent(params: Overwrite<Partial<gh.ReposGetContentPar
   }
 }
 
-async function _markCheckAsInProgress(context: Context): Promise<Context> {
+async function markCheckAsInProgress(context: Context): Promise<Context> {
   const result = await promiseChecks.update(context, created2ChecksUpdateParams({status: "in_progress"}));
   return result.context
 }
 
-function markCheckAsInProgress(source: rx.Observable<Context>): rx.Observable<Context> {
-  return source.pipe(
-    flatMap(context => rxChecks.update(context, created2ChecksUpdateParams({status: "in_progress"}))),
-    map(data => data.context)
-    )
-}
-
-async function _fetchModifiedFiles(context: Context): Promise<{context: Context, files: string[]}> {
+async function fetchModifiedFiles(context: Context): Promise<{context: Context, files: string[]}> {
   const commits = await promiseRepos.compareCommits(context, created2ReposCompareCommitsParams)
   const files: string[] = commits.response.data.files.filter((file: any) => file.status !== "deleted").map((file: any) => file.filename)
   return {context, files}
 }
 
-function fetchModifiedFiles(source: rx.Observable<Context>): rx.Observable<{context: Context, file: string}> {
-  const files = source.pipe(
-    flatMap(context => rxRepos.compareCommits(context, created2ReposCompareCommitsParams)),
-    flatMap(data => rx.from(data.response.data.files.filter((file: any) => file.status != "deleted").map((file: any) => file.filename) as string[]))
-  )
-  return rx.combineLatest(source, files, (context, file) => ({context, file}))
-}
-
-async function _fetchContent(data: {context: Context, file: string}): Promise<{context: Context, file: string, content: string}> {
+async function fetchContent(data: {context: Context, file: string}): Promise<{context: Context, file: string, content: string}> {
   const result = await promiseRepos.getContent(data.context, created2ReposGetContent({path: data.file}))
   if(result.response.data.encoding === 'base64') {
     const content = btoa(result.response.data);
@@ -97,51 +67,21 @@ async function _fetchContent(data: {context: Context, file: string}): Promise<{c
   }
 }
 
-function fetchContent(source: rx.Observable<{context: Context, file: string}>): rx.Observable<{context: Context, file: string, content: string}> {
-  const content = source.pipe(
-    flatMap(data => rxRepos.getContent(data.context, created2ReposGetContent({path: data.file}))),
-    map(data => data.response.data)
-  )
-  return rx.combineLatest(source, content, (data, content) => ({context: data.context, file: data.file, content}))
-}
-
-async function _checkContent(data: {context: Context, file: string, content: string}): Promise<{context: Context, file: string, passed: boolean}> {
+async function checkContent(data: {context: Context, file: string, content: string}): Promise<{context: Context, file: string, passed: boolean}> {
   const passed = await prettier.check(data.content, {filepath: data.file})
   return {context: data.context, file: data.file, passed}
 }
 
-function checkContent(source: rx.Observable<{context: Context, file: string, content: string}>): rx.Observable<{context: Context, file: string, passed: boolean}> {
-  const passedCheck = source.pipe(
-    flatMap(data => rxPrettier.check(data.content, {filepath: data.file}))
-  )
-  return rx.combineLatest(source, passedCheck, (data, passed) => ({context: data.context, file: data.file, passed}))
-}
-
-function compileReport(source: rx.Observable<{context: Context, file: string, passed: boolean}>): rx.Observable<{context: Context, report: Partial<gh.ChecksUpdateParams>}> {
-  const report = source.pipe(
-    reduce<{context: Context, file: string, passed: boolean}, {file: string, passed: boolean}[]>((acc, data) => [...acc, {file: data.file, passed: data.passed}], []),
-    map(results => asReport(results))
-  )
-  return rx.combineLatest(source, report, (data, report) => ({context: data.context, report}))
-}
-
-async function _markCheckAsCompleted(data: {context: Context, report: Partial<gh.ChecksUpdateParams>}): Promise<Context> {
+async function markCheckAsCompleted(data: {context: Context, report: Partial<gh.ChecksUpdateParams>}): Promise<Context> {
   const completed_at = new Date().toISOString()
   const result = await promiseChecks.update(data.context, created2ChecksUpdateParams({...data.report, status: "completed", completed_at}))
   return result.context
 }
 
-function markCheckAsCompleted(source: rx.Observable<{context: Context, report: Partial<gh.ChecksUpdateParams>}>): rx.Observable<Context> {
-  return source.pipe(
-    flatMap(data => rxChecks.update(data.context, created2ChecksUpdateParams({...data.report, status: "completed", completed_at: new Date().toISOString()}))),
-    map(data => data.context)
-    )
-}
 
-
-export function rerequested(context: Context): rx.Observable<Context> {
-  return rxChecks.create(context, rerequested2ChecksCreateParams)
-  .pipe(map(r => r.context))
+export async function rerequested(context: Context): Promise<Context> {
+   await promiseChecks.create(context, rerequested2ChecksCreateParams)
+   return context
 }
 
 function rerequested2ChecksCreateParams(context: Context): gh.ChecksCreateParams {
--- src/events/check_suite.ts
+++ src/events/check_suite.ts
@@ -1,14 +1,12 @@
-import * as rx from "rxjs";
 import * as gh from "@octokit/rest";
-import { rxChecks} from "../reactivex/rxGithub"
+import { promiseChecks} from "../reactivex/rxGithub"
 import { Context } from 'probot'
 
 import { CHECKS_NAME } from '../utils';
-import { map } from 'rxjs/operators';
 
-export function requested (context: Context): rx.Observable<Context> {
-  return rxChecks.create(context, requested2ChecksCreateParams)
-  .pipe(map(r => r.context))
+export async function requested (context: Context): Promise<Context> {
+  await promiseChecks.create(context, requested2ChecksCreateParams)
+  return context
 }
 
 function requested2ChecksCreateParams(context: Context): gh.ChecksCreateParams {
