0be7bceebc6535b1a77a0bb8554735785118ef46
Claas Ahlrichs <claasahl@gmail.com>
2018-09-29 07:02:22 +0200

    refined test cases for "fix" action

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,7 @@
 import { Application } from 'probot'
 import myProbotApp from '../src/index'
 import * as gh from "@octokit/rest";
-import { advanceBy, advanceTo } from 'jest-date-mock';
+import { advanceTo } from 'jest-date-mock';
 
 import checkSuiteRequestedEvent from "./events/check_suite/requested.json"
 import checkSuiteRerequestedEvent from "./events/check_suite/rerequested.json"
@@ -13,13 +13,14 @@ import checkRunCreatedEvent from "./events/check_run/created.json"
 
 import reposCompareCommits_200 from "./responses/repos/compareCommits__200.json"
 import reposgetContent_200 from "./responses/repos/getContent__200.json"
-import reposgetContent_200_unformatted from "./responses/repos/getContent__200__unformatted.json"
 import reposUpdateFile_200 from "./responses/repos/updateFile__200.json"
 import gitdataCreateReference_201 from "./responses/gitdata/createReference__201.json"
 import pullRequestsCreate_201 from "./responses/pullRequests/create__201.json"
 
 import { CHECKS_NAME } from '../src/utils';
 import template from './templates/pullRequest';
+import * as fs from 'fs';
+import * as prettier from "prettier"
 
 describe('tests conditions for triggering a file-analysis', () => {
   let app, github
@@ -129,8 +130,8 @@ describe("test for pull request (fix of errors in check_run)", () => {
     // This is an easy way to mock out the GitHub API
     github = {
       repos: {
-        compareCommits: jest.fn().mockResolvedValue(reposCompareCommits_200),
-        getContent: jest.fn().mockResolvedValue(reposgetContent_200),
+        compareCommits: jest.fn().mockImplementation(mockReposCompareCommits("./test/templates/unformatted.json")),
+        getContent: jest.fn().mockImplementation(mockReposGetContentFormatted),
         updateFile: jest.fn().mockResolvedValue(reposUpdateFile_200)
       },
       gitdata: {
@@ -161,15 +162,25 @@ describe("test for pull request (fix of errors in check_run)", () => {
   test('retrieves contents for file-version for relevant check_run', async () => {
     await app.receive(checkRunRequestedActionEvent)
 
-    const params: gh.ReposGetContentParams = { owner: "claasahl", repo: "prettiest-bot", path: "test/utils.test.ts", ref: "refs/heads/prettier/develop"};
+    const params: gh.ReposGetContentParams = { owner: "claasahl", repo: "prettiest-bot", path: "./test/templates/unformatted.json", ref: "refs/heads/prettier/develop"};
     expect(github.repos.getContent).toHaveBeenCalledWith(params)
   })
 
-  test('create pull request for fixed files', async () => {
-    github.repos.getContent = jest.fn().mockResolvedValue(reposgetContent_200_unformatted)
+  test('create pull request for fixed file (singular)', async () => {
+    github.repos.getContent = jest.fn().mockImplementation(mockReposGetContentUnformatted)
     await app.receive(checkRunRequestedActionEvent)
 
     const body = template("https://github.com/claasahl/prettiest-bot/runs/16344324", 1, "claasahl")
+    const params: gh.PullRequestsCreateParams = { owner: "claasahl", repo: "prettiest-bot", base: "develop", head: "refs/heads/prettier/develop", title: "Prettified branch: develop", maintainer_can_modify: true, body};
+    expect(github.pullRequests.create).toHaveBeenCalledWith(params)
+  })
+
+  test('create pull request for fixed files (plural)', async () => {
+    github.repos.compareCommits = jest.fn().mockImplementation(mockReposCompareCommits("./test/templates/unformatted.ts", "./test/templates/unformatted.json"))
+    github.repos.getContent = jest.fn().mockImplementation(mockReposGetContentUnformatted)
+    await app.receive(checkRunRequestedActionEvent)
+
+    const body = template("https://github.com/claasahl/prettiest-bot/runs/16344324", 2, "claasahl")
     const params: gh.PullRequestsCreateParams = { owner: "claasahl", repo: "prettiest-bot", base: "develop", head: "refs/heads/prettier/develop", title: "Prettified branch: develop", maintainer_can_modify: true, body};
     expect(github.pullRequests.create).toHaveBeenCalledWith(params)
   })
@@ -183,13 +194,49 @@ describe("test for pull request (fix of errors in check_run)", () => {
   })
 
   test('format and update files that need fixing', async () => {
-    github.repos.getContent = jest.fn().mockResolvedValue(reposgetContent_200_unformatted)
+    github.repos.compareCommits = jest.fn().mockImplementation(mockReposCompareCommits("./test/templates/unformatted.ts", "./test/templates/unformatted.json"))
+    github.repos.getContent = jest.fn().mockImplementation(mockReposGetContentUnformatted)
     await app.receive(checkRunRequestedActionEvent)
 
-    const content = "Ly8gWW91IGNhbiBpbXBvcnQgeW91ciBtb2R1bGVzCi8vIGltcG9ydCBpbmRleCBmcm9tICcuLi9zcmMvaW5kZXgnCgppbXBvcnQgKiBhcyBSeCBmcm9tICJyeGpzIjsKaW1wb3J0IHsgb2ZFdmVudCwgb2ZFdmVudEZpbHRlciB9IGZyb20gIi4uL3NyYy91dGlscyI7CgpkZXNjcmliZSgib2ZFdmVudEZpbHRlciAvIG9mRXZlbnQiLCAoKSA9PiB7CiAgdGVzdCgidGhhdCAnKicgbWF0Y2hlcyBhbnkgZXZlbnQiLCBhc3luYyAoKSA9PiB7CiAgICBleHBlY3Qob2ZFdmVudEZpbHRlcigiKiIpKGZha2VFdmVudCgic29tZSBldmVudCIsICJzb21lIGFjdGlvbiIpKSkudG9CZSgKICAgICAgdHJ1ZQogICAgKTsKICAgIGV4cGVjdChvZkV2ZW50RmlsdGVyKCIqIikoZmFrZUV2ZW50KCJzb21lIG90aGVyIGV2ZW50IikpKS50b0JlKHRydWUpOwogIH0pOwoKICB0ZXN0KCJ0aGF0IGV2ZW50IG5hbWUgbWF0Y2hlcyIsIGFzeW5jICgpID0+IHsKICAgIGV4cGVjdChvZkV2ZW50RmlsdGVyKCJwdXNoIikoZmFrZUV2ZW50KCJwdXNoIiwgInNvbWUgYWN0aW9uIikpKS50b0JlKHRydWUpOwogICAgZXhwZWN0KG9mRXZlbnRGaWx0ZXIoInB1c2giKShmYWtlRXZlbnQoInB1c2giKSkpLnRvQmUodHJ1ZSk7CiAgICBleHBlY3Qob2ZFdmVudEZpbHRlcigicHVzaCIpKGZha2VFdmVudCgiY2hlY2tfc3VpdGUiKSkpLnRvQmUoZmFsc2UpOwogIH0pOwoKICB0ZXN0KCJ0aGF0IGV2ZW50IG5hbWUgYW5kIGFjdGlvbiBtYXRjaCIsIGFzeW5jICgpID0+IHsKICAgIGV4cGVjdCgKICAgICAgb2ZFdmVudEZpbHRlcigiY2hlY2tfcnVuLmNyZWF0ZWQiKShmYWtlRXZlbnQoImNoZWNrX3J1biIsICJjcmVhdGVkIikpCiAgICApLnRvQmUodHJ1ZSk7CiAgICBleHBlY3QoCiAgICAgIG9mRXZlbnRGaWx0ZXIoImNoZWNrX3J1bi5jcmVhdGVkIikoZmFrZUV2ZW50KCJjaGVja19ydW4iLCAicmVyZXF1ZXN0ZWQiKSkKICAgICkudG9CZShmYWxzZSk7CiAgICBleHBlY3Qob2ZFdmVudEZpbHRlcigiY2hlY2tfcnVuLmNyZWF0ZWQiKShmYWtlRXZlbnQoImNoZWNrX3J1biIpKSkudG9CZSgKICAgICAgZmFsc2UKICAgICk7CiAgfSk7CgogIHRlc3QoInRoYXQgbWF0Y2hlZCBldmVudHMgYXJlIGtlcHQiLCBhc3luYyAoKSA9PiB7CiAgICBjb25zdCBldmVudDEgPSBmYWtlRXZlbnQoInB1c2giLCAiYWN0aW9uIik7CiAgICBjb25zdCBldmVudDIgPSBmYWtlRXZlbnQoImFub3RoZXIgZXZlbnQiKTsKICAgIGNvbnN0IHJlc3VsdCA9IGF3YWl0IFJ4Lm9mKGV2ZW50MSwgZXZlbnQyKQogICAgICAucGlwZShvZkV2ZW50KCJwdXNoIikpCiAgICAgIC50b1Byb21pc2UoKTsKICAgIGV4cGVjdChyZXN1bHQpLnRvQmUoZXZlbnQxKTsKICB9KTsKCiAgdGVzdCgidGhhdCB1bm1hdGNoZWQgZXZlbnRzIGFyZSBkcm9wcGVkIiwgYXN5bmMgKCkgPT4gewogICAgY29uc3QgZXZlbnQxID0gZmFrZUV2ZW50KCJldmVudCIsICJhY3Rpb24iKTsKICAgIGNvbnN0IGV2ZW50MiA9IGZha2VFdmVudCgiYW5vdGhlciBldmVudCIpOwogICAgY29uc3QgcmVzdWx0ID0gYXdhaXQgUngub2YoZXZlbnQxLCBldmVudDIpCiAgICAgIC5waXBlKG9mRXZlbnQoInB1c2giKSkKICAgICAgLnRvUHJvbWlzZSgpOwogICAgZXhwZWN0KHJlc3VsdCkudG9CZSh1bmRlZmluZWQpOwogIH0pOwoKICBmdW5jdGlvbiBmYWtlRXZlbnQobmFtZTogc3RyaW5nLCBhY3Rpb24/OiBzdHJpbmcpIHsKICAgIHJldHVybiB7IGV2ZW50OiBuYW1lLCBwYXlsb2FkOiB7IGFjdGlvbiB9IH07CiAgfQp9KTsK"
-    const params: gh.ReposUpdateFileParams = { owner: "claasahl", repo: "prettiest-bot", message: "formatted file: test/utils.test.ts", path: "test/utils.test.ts", branch: "refs/heads/prettier/develop", sha: "7d6e8ee3e4c26b6d5d305fa3fe985ddf7c0f87ea", content};
+    let content = btoa(prettier.format(fs.readFileSync("./test/templates/unformatted.ts").toString(), {filepath: "./test/templates/unformatted.ts"}))
+    let params: gh.ReposUpdateFileParams = { owner: "claasahl", repo: "prettiest-bot", message: "formatted file: ./test/templates/unformatted.ts", path: "./test/templates/unformatted.ts", branch: "refs/heads/prettier/develop", sha: "7d6e8ee3e4c26b6d5d305fa3fe985ddf7c0f87ea", content};
+    expect(github.repos.updateFile).toHaveBeenCalledWith(params)
+
+    content = btoa(prettier.format(fs.readFileSync("./test/templates/unformatted.json").toString(), {filepath: "./test/templates/unformatted.json"}))
+    params = { owner: "claasahl", repo: "prettiest-bot", message: "formatted file: ./test/templates/unformatted.json", path: "./test/templates/unformatted.json", branch: "refs/heads/prettier/develop", sha: "7d6e8ee3e4c26b6d5d305fa3fe985ddf7c0f87ea", content};
     expect(github.repos.updateFile).toHaveBeenCalledWith(params)
   })
+
+  function mockReposCompareCommits(...files: string[]) {
+    return async () => {
+      const base = reposCompareCommits_200;
+      base.data.files = files.map(file => ({filename: file, status: "modified"}))
+      return base
+    }
+  }
+
+  async function mockReposGetContentFormatted(params: gh.ReposGetContentParams) {
+    const base = reposgetContent_200;
+    if(fs.existsSync(params.path)) {
+      const unformatted = fs.readFileSync(params.path).toString()
+      const formatted = prettier.format(unformatted, {filepath: params.path})
+      base.data.name = params.path
+      base.data.path = params.path
+      base.data.content = btoa(formatted)
+    }
+    return base
+  }
+
+  async function mockReposGetContentUnformatted(params: gh.ReposGetContentParams) {
+    const base = reposgetContent_200;
+    if(fs.existsSync(params.path)) {
+      const unformatted = fs.readFileSync(params.path).toString()
+      base.data.name = params.path
+      base.data.path = params.path
+      base.data.content = btoa(unformatted)
+    }
+    return base
+  }
 })
 
 // For more information about using TypeScript in your tests, Jest recommends:
