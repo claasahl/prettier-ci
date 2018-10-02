9615c5fdbb2e37540e45c77c5e256b1e4ab7a809
Claas Ahlrichs <claasahl@gmail.com>
2018-10-02 07:05:11 +0200

    changed name to "prettier-ci"

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# prettiest-bot
+# prettier-ci
 
 > A GitHub App built with [Probot](https://github.com/probot/probot) that A Probot app
 
@@ -17,7 +17,7 @@ npm start
 
 ## Contributing
 
-If you have suggestions for how prettiest-bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+If you have suggestions for how prettier-ci could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
 
 For more, check out the [Contributing Guide](CONTRIBUTING.md).
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -44,21 +44,21 @@ describe('tests conditions for triggering a file-analysis', () => {
   test('creates check when check_suite is requested', async () => {
     await app.receive(checkSuiteRequestedEvent)
 
-    const params: gh.ChecksCreateParams = { owner: "claasahl", repo: "prettiest-bot", name: CHECKS_NAME, head_sha: "39a53e909e101414ee8880321bf0079e4dd7d767" };
+    const params: gh.ChecksCreateParams = { owner: "claasahl", repo: "prettier-ci", name: CHECKS_NAME, head_sha: "39a53e909e101414ee8880321bf0079e4dd7d767" };
     expect(github.checks.create).toHaveBeenCalledWith(params)
   })
 
   test('creates check when check_suite is rerequested', async () => {
     await app.receive(checkSuiteRerequestedEvent)
 
-    const params: gh.ChecksCreateParams = { owner: "claasahl", repo: "prettiest-bot", name: CHECKS_NAME, head_sha: "bb4d2ed9702c4c4340db50f74fc451657fe48e57" };
+    const params: gh.ChecksCreateParams = { owner: "claasahl", repo: "prettier-ci", name: CHECKS_NAME, head_sha: "bb4d2ed9702c4c4340db50f74fc451657fe48e57" };
     expect(github.checks.create).toHaveBeenCalledWith(params)
   })
 
   test('creates check when check_run is rerequested', async () => {
     await app.receive(checkRunRerequestedEvent)
 
-    const params: gh.ChecksCreateParams = { owner: "claasahl", repo: "prettiest-bot", name: CHECKS_NAME, head_sha: "fad5b3a8a9602fd31279d8c707e5a0de3c4cd640" };
+    const params: gh.ChecksCreateParams = { owner: "claasahl", repo: "prettier-ci", name: CHECKS_NAME, head_sha: "fad5b3a8a9602fd31279d8c707e5a0de3c4cd640" };
     expect(github.checks.create).toHaveBeenCalledWith(params)
   })
 })
@@ -88,14 +88,14 @@ describe('tests for file-analysis (check_run)', () => {
   test('mark check as "in_progress" when check_run is created', async () => {
     await app.receive(checkRunCreatedEvent)
 
-    const params: gh.ChecksUpdateParams = { owner: "claasahl", repo: "prettiest-bot", check_run_id: "15222485", status: "in_progress"};
+    const params: gh.ChecksUpdateParams = { owner: "claasahl", repo: "prettier-ci", check_run_id: "15222485", status: "in_progress"};
     expect(github.checks.update).toHaveBeenCalledWith(params)
   })
 
   test('compares before/after commits for check_run', async () => {
     await app.receive(checkRunCreatedEvent)
 
-    const params: gh.ReposCompareCommitsParams = { owner: "claasahl", repo: "prettiest-bot", base: "923547128e3e9fe26c4192bb45fcf125dae92c4b", head: "52c67357a6b1858226c131d13d7d44e8303fd426"};
+    const params: gh.ReposCompareCommitsParams = { owner: "claasahl", repo: "prettier-ci", base: "923547128e3e9fe26c4192bb45fcf125dae92c4b", head: "52c67357a6b1858226c131d13d7d44e8303fd426"};
     expect(github.repos.compareCommits).toHaveBeenCalledWith(params)
   })
 
@@ -105,7 +105,7 @@ describe('tests for file-analysis (check_run)', () => {
     // TODO app.receive already completes when the event has been received, but not when then event has been processed
     await new Promise(resolve => setTimeout(resolve, 2000))
 
-    const params: gh.ReposGetContentParams = { owner: "claasahl", repo: "prettiest-bot", path: "test/utils.test.ts", ref: "52c67357a6b1858226c131d13d7d44e8303fd426"};
+    const params: gh.ReposGetContentParams = { owner: "claasahl", repo: "prettier-ci", path: "test/utils.test.ts", ref: "52c67357a6b1858226c131d13d7d44e8303fd426"};
     expect(github.repos.getContent).toHaveBeenCalledWith(params)
   })
 
@@ -116,7 +116,7 @@ describe('tests for file-analysis (check_run)', () => {
     // TODO app.receive already completes when the event has been received, but not when then event has been processed
     await new Promise(resolve => setTimeout(resolve, 2000))
 
-    const params: gh.ChecksUpdateParams = { owner: "claasahl", repo: "prettiest-bot", check_run_id: "15222485", status: "completed", conclusion: "success", output: {summary: "Pretty. Keep up the **good work**.", title: "Prettier", text: undefined}, actions: [], completed_at: new Date().toISOString()};
+    const params: gh.ChecksUpdateParams = { owner: "claasahl", repo: "prettier-ci", check_run_id: "15222485", status: "completed", conclusion: "success", output: {summary: "Pretty. Keep up the **good work**.", title: "Prettier", text: undefined}, actions: [], completed_at: new Date().toISOString()};
     expect(github.checks.update).toHaveBeenLastCalledWith(params)
   })
 })
@@ -150,21 +150,21 @@ describe("test for pull request (fix of errors in check_run)", () => {
   test("'fix' action should create branch", async () => {
     await app.receive(checkRunRequestedActionEvent)
 
-    const params: gh.GitdataCreateReferenceParams = {owner: "claasahl", repo: "prettiest-bot", ref: "refs/heads/prettier/develop", sha: "bb4d2ed9702c4c4340db50f74fc451657fe48e57"}
+    const params: gh.GitdataCreateReferenceParams = {owner: "claasahl", repo: "prettier-ci", ref: "refs/heads/prettier/develop", sha: "bb4d2ed9702c4c4340db50f74fc451657fe48e57"}
     expect(github.gitdata.createReference).toHaveBeenCalledWith(params)
   })
 
   test('compares before/after commits for relevant check_run', async () => {
     await app.receive(checkRunRequestedActionEvent)
 
-    const params: gh.ReposCompareCommitsParams = { owner: "claasahl", repo: "prettiest-bot", base: "a137828f79b51c516391b0d125b8df492fdfb3ce", head: "bb4d2ed9702c4c4340db50f74fc451657fe48e57"};
+    const params: gh.ReposCompareCommitsParams = { owner: "claasahl", repo: "prettier-ci", base: "a137828f79b51c516391b0d125b8df492fdfb3ce", head: "bb4d2ed9702c4c4340db50f74fc451657fe48e57"};
     expect(github.repos.compareCommits).toHaveBeenCalledWith(params)
   })
 
   test('retrieves contents for file-version for relevant check_run', async () => {
     await app.receive(checkRunRequestedActionEvent)
 
-    const params: gh.ReposGetContentParams = { owner: "claasahl", repo: "prettiest-bot", path: "./test/templates/unformatted.json", ref: "refs/heads/prettier/develop"};
+    const params: gh.ReposGetContentParams = { owner: "claasahl", repo: "prettier-ci", path: "./test/templates/unformatted.json", ref: "refs/heads/prettier/develop"};
     expect(github.repos.getContent).toHaveBeenCalledWith(params)
   })
 
@@ -172,8 +172,8 @@ describe("test for pull request (fix of errors in check_run)", () => {
     github.repos.getContent = jest.fn().mockImplementation(mockReposGetContentUnformatted)
     await app.receive(checkRunRequestedActionEvent)
 
-    const body = template("https://github.com/claasahl/prettiest-bot/runs/16344324", 1, "claasahl")
-    const params: gh.PullRequestsCreateParams = { owner: "claasahl", repo: "prettiest-bot", base: "develop", head: "refs/heads/prettier/develop", title: "Prettified branch: develop", maintainer_can_modify: true, body};
+    const body = template("https://github.com/claasahl/prettier-ci/runs/16344324", 1, "claasahl")
+    const params: gh.PullRequestsCreateParams = { owner: "claasahl", repo: "prettier-ci", base: "develop", head: "refs/heads/prettier/develop", title: "Prettified branch: develop", maintainer_can_modify: true, body};
     expect(github.pullRequests.create).toHaveBeenCalledWith(params)
   })
 
@@ -182,16 +182,16 @@ describe("test for pull request (fix of errors in check_run)", () => {
     github.repos.getContent = jest.fn().mockImplementation(mockReposGetContentUnformatted)
     await app.receive(checkRunRequestedActionEvent)
 
-    const body = template("https://github.com/claasahl/prettiest-bot/runs/16344324", 2, "claasahl")
-    const params: gh.PullRequestsCreateParams = { owner: "claasahl", repo: "prettiest-bot", base: "develop", head: "refs/heads/prettier/develop", title: "Prettified branch: develop", maintainer_can_modify: true, body};
+    const body = template("https://github.com/claasahl/prettier-ci/runs/16344324", 2, "claasahl")
+    const params: gh.PullRequestsCreateParams = { owner: "claasahl", repo: "prettier-ci", base: "develop", head: "refs/heads/prettier/develop", title: "Prettified branch: develop", maintainer_can_modify: true, body};
     expect(github.pullRequests.create).toHaveBeenCalledWith(params)
   })
 
   test('create "empty" pull request when no files need fixing', async () => {
     await app.receive(checkRunRequestedActionEvent)
 
-    const body = template("https://github.com/claasahl/prettiest-bot/runs/16344324", 0, "claasahl")
-    const params: gh.PullRequestsCreateParams = { owner: "claasahl", repo: "prettiest-bot", base: "develop", head: "refs/heads/prettier/develop", title: "Prettified branch: develop", maintainer_can_modify: true, body};
+    const body = template("https://github.com/claasahl/prettier-ci/runs/16344324", 0, "claasahl")
+    const params: gh.PullRequestsCreateParams = { owner: "claasahl", repo: "prettier-ci", base: "develop", head: "refs/heads/prettier/develop", title: "Prettified branch: develop", maintainer_can_modify: true, body};
     expect(github.pullRequests.create).toHaveBeenCalledWith(params)
   })
 
@@ -201,11 +201,11 @@ describe("test for pull request (fix of errors in check_run)", () => {
     await app.receive(checkRunRequestedActionEvent)
 
     let content = btoa(prettier.format(fs.readFileSync("./test/templates/unformatted.ts").toString(), {filepath: "./test/templates/unformatted.ts"}))
-    let params: gh.ReposUpdateFileParams = { owner: "claasahl", repo: "prettiest-bot", message: "formatted file: ./test/templates/unformatted.ts", path: "./test/templates/unformatted.ts", branch: "refs/heads/prettier/develop", sha: "7d6e8ee3e4c26b6d5d305fa3fe985ddf7c0f87ea", content};
+    let params: gh.ReposUpdateFileParams = { owner: "claasahl", repo: "prettier-ci", message: "formatted file: ./test/templates/unformatted.ts", path: "./test/templates/unformatted.ts", branch: "refs/heads/prettier/develop", sha: "7d6e8ee3e4c26b6d5d305fa3fe985ddf7c0f87ea", content};
     expect(github.repos.updateFile).toHaveBeenCalledWith(params)
 
     content = btoa(prettier.format(fs.readFileSync("./test/templates/unformatted.json").toString(), {filepath: "./test/templates/unformatted.json"}))
-    params = { owner: "claasahl", repo: "prettiest-bot", message: "formatted file: ./test/templates/unformatted.json", path: "./test/templates/unformatted.json", branch: "refs/heads/prettier/develop", sha: "7d6e8ee3e4c26b6d5d305fa3fe985ddf7c0f87ea", content};
+    params = { owner: "claasahl", repo: "prettier-ci", message: "formatted file: ./test/templates/unformatted.json", path: "./test/templates/unformatted.json", branch: "refs/heads/prettier/develop", sha: "7d6e8ee3e4c26b6d5d305fa3fe985ddf7c0f87ea", content};
     expect(github.repos.updateFile).toHaveBeenCalledWith(params)
   })
 
@@ -214,7 +214,7 @@ describe("test for pull request (fix of errors in check_run)", () => {
     github.repos.getContent = jest.fn().mockImplementation(mockReposGetContentUnformatted)
     await app.receive(checkRunRequestedActionEvent)
 
-    const params: gh.PullRequestsMergeParams = { owner: "claasahl", repo: "prettiest-bot", number: 3};
+    const params: gh.PullRequestsMergeParams = { owner: "claasahl", repo: "prettier-ci", number: 3};
     expect(github.pullRequests.merge).toHaveBeenCalledWith(params)
   })
 
