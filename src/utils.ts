6acbc8f4247f3d1f90d307750fed945b0bd3103b
Claas Ahlrichs <claasahl@gmail.com>
2018-10-03 07:05:49 +0200

    updated: utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,4 @@
-export const CHECKS_NAME = "Prettier"
+export const CHECKS_NAME = "Prettier-CI"
 export const REFERENCE_PREFIX = "refs/heads/prettier/"
 export const COMMIT_MESSAGE_PREFIX = "formatted file: ";
 export const PULL_REQUEST_TITLE_PREFIX = "Prettified branch: "
-export const USER_NAME = process.env.name || 'prettier'
-export const USER_TYPE = 'bot'
