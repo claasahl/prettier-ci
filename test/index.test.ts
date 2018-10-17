a6342f9cd9ce99ec7464a286694f45d4ce049e8e
Claas Ahlrichs <claasahl@gmail.com>
2018-10-17 06:34:46 +0200

    cleaned up test env.

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,12 +2,7 @@ import { Application } from 'probot'
 // Requiring our app implementation
 import myProbotApp from '../src'
 
-const issuesOpenedPayload = require('./fixtures/issues.opened.json')
-
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
-})
+import checkSuiteRequested from './fixtures/events/check_suite.requested.json';
 
 describe('My Probot app', () => {
   let app: Application
@@ -19,8 +14,8 @@ describe('My Probot app', () => {
     app.load(myProbotApp)
     // This is an easy way to mock out the GitHub API
     github = {
-      issues: {
-        createComment: jest.fn().mockReturnValue(Promise.resolve({}))
+      checks: {
+        create: jest.fn().mockResolvedValue(0),
       }
     }
     // Passes the mocked out GitHub API into out app instance
@@ -30,12 +25,12 @@ describe('My Probot app', () => {
   test('creates a comment when an issue is opened', async () => {
     // Simulates delivery of an issues.opened webhook
     await app.receive({
-      name: 'issues.opened',
-      payload: issuesOpenedPayload
+      name: 'check_suite.requested',
+      payload: checkSuiteRequested
     })
 
     // This test passes if the code in your index.ts file calls `context.github.issues.createComment`
-    expect(github.issues.createComment).toHaveBeenCalled()
+    expect(github.checks.create).toHaveBeenCalled()
   })
 })
 
