7eecc0d35d3e8a7614b35c7a33eb07b7acc148ee
Claas Ahlrichs <claasahl@gmail.com>
2018-10-23 06:40:38 +0200

    managed to mock shelljs

--- test/events/check_run.test.ts
+++ test/events/check_run.test.ts
@@ -6,15 +6,19 @@ import * as mockdate from "mockdate";
 import * as CheckRun from "../../src/events/check_run";
 import CheckRunRerequested from "../fixtures/events/check_run.rerequested.json";
 import CheckRunCreated from "../fixtures/events/check_run.created.json";
-import { createParams } from "../../src/checks_params";
+import * as checks_params from "../../src/checks_params";
 
 import * as git from "isomorphic-git";
 jest.mock("isomorphic-git");
 const mockedGit = mocked(git);
 
-// import * as shelljs from "shelljs";
-// jest.mock("shelljs")
-// const mockedShelljs = mocked(shelljs)
+import * as shelljs from "shelljs";
+jest.mock("shelljs", () => ({
+  test: jest.fn(),
+  exec: jest.fn(),
+  rm: jest.fn()
+}))
+const mockedShelljs = mocked(shelljs)
 
 describe("tests for 'check_run.*'-events", async () => {
   const github: any = {
@@ -29,7 +33,7 @@ describe("tests for 'check_run.*'-events", async () => {
     await CheckRun.rerequested(new Context(CheckRunRerequested, github, log));
     expect(github.checks.create).toHaveBeenCalledTimes(1);
     expect(github.checks.create).toHaveBeenCalledWith({
-      ...createParams(),
+      ...checks_params.createParams(),
       owner: "username",
       repo: "repository",
       head_sha: "CCCCCCCCCCCCCCCCCCCCCCCCCC"
@@ -37,28 +41,31 @@ describe("tests for 'check_run.*'-events", async () => {
   });
 
   test("'.created' should create 'check_run'", async () => {
-    // mockedShelljs.test.mockReturnValue(false)
+    mockedShelljs.test.mockReturnValue(false)
+    mockedShelljs.exec.mockReturnValue({stderr: `
+whatever.bla
+`, stdout: `
+bla.js
+`})
+    
     const completed_at = "2010-05-28T15:29:41.839Z";
     mockdate.set(completed_at);
     await CheckRun.created(new Context(CheckRunCreated, github, log));
-    mockdate.reset();
-
+    
     expect(github.checks.update).toHaveBeenCalledTimes(2);
     expect(github.checks.update).toHaveBeenCalledWith({
+      ...checks_params.inProgressParams(),
       check_run_id: 42,
-      status: "in_progress",
       owner: "username",
       repo: "repository"
     });
     expect(github.checks.update).toHaveBeenCalledWith({
+      ...checks_params.failureParams(["whatever.bla"],[],["bla.js"]),
       check_run_id: 42,
-      status: "completed",
       owner: "username",
       repo: "repository",
-      conclusion: "failure",
-      completed_at
     });
-
+    
     expect(mockedGit.clone).toHaveBeenCalledTimes(1);
     expect(mockedGit.clone).toBeCalledWith({
       dir: "./repos/username/repository",
@@ -69,9 +76,10 @@ describe("tests for 'check_run.*'-events", async () => {
       dir: "./repos/username/repository",
       ref: "DDDDDDDDDDDDDDD"
     });
-
-    // expect(mockedShelljs.test).toHaveBeenCalledTimes(1)
-    // expect(mockedShelljs.exec).toHaveBeenCalledTimes(1)
-    // expect(mockedShelljs.rm).toHaveBeenCalledTimes(1)
+    
+    expect(mockedShelljs.test).toHaveBeenCalledTimes(1)
+    expect(mockedShelljs.exec).toHaveBeenCalledTimes(1)
+    expect(mockedShelljs.rm).toHaveBeenCalledTimes(1)
+    mockdate.reset();
   });
 });
