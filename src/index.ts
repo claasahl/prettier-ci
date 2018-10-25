e6786f1eb4c78d5cedbd1d64c684d79a40ab36a9
Claas Ahlrichs <claasahl@gmail.com>
2018-10-25 06:38:43 +0200

    drafted "lstat" implementation for memory-fs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Application } from "probot";
 import * as CheckSuite from "./events/check_suite";
 import * as CheckRun from "./events/check_run";
+import MemoryFileSystem from "memory-fs";
 
 export = (app: Application) => {
   // Your code here
@@ -20,3 +21,8 @@ export = (app: Application) => {
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
 };
+
+// add lstat + lstatSync to memory-fs
+// see https://linux.die.net/man/2/lstat
+(MemoryFileSystem as any).prototype["lstatSync"] = MemoryFileSystem.prototype.statSync;
+(MemoryFileSystem as any).prototype["lstat"] = MemoryFileSystem.prototype.stat;
